cc8c9fefaee442e953eea07c908fff7f9bf84bed
FIRST_NAME LAST_NAME <j.liu484@gmail.com>
2023-02-05 17:12:07 -0500

    import webmidi and get it to display the notes played on jeff's piano

--- wk1/sketch.js
+++ wk1/sketch.js
@@ -1,5 +1,35 @@
 function setup() {
-  // put setup code here
+  // setup to enable WebMidi library: https://webmidijs.org/docs/
+  WebMidi
+      .enable()
+      .then(onEnabled)
+      .catch(err => alert(err));
+  
+    // Function triggered when WEBMIDI.js is ready
+    function onEnabled() {
+  
+      // Inputs
+      WebMidi.inputs.forEach(input => console.log(input.manufacturer, input.name));
+      // Outputs
+      WebMidi.outputs.forEach(output => console.log(output.manufacturer, output.name));
+
+      // Display available MIDI input devices
+      if (WebMidi.inputs.length < 1) {
+        document.body.innerHTML+= "No device detected.";
+      } else {
+        WebMidi.inputs.forEach((device, index) => {
+          document.body.innerHTML+= `${index}: ${device.name} <br>`;
+        });
+      }
+  
+      const mySynth = WebMidi.inputs[0];
+
+      // jeff's piano shows all info coming from channel 16: display e.message.channel
+      mySynth.addListener("noteon", e => {
+        // console.log(e.note.identifier);
+        document.body.innerHTML+= `${e.note.identifier} <br>`;
+      })
+    }
 }
 
 function draw() {
